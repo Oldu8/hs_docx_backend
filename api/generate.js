d96b1258c30709c1bb5fcb19568e88327b838518
Oldu8 <dudkoleg@gmail.com>
2025-06-25 17:11:05 +0200

    split code, upd templates

--- api/generate.js
+++ api/generate.js
@@ -2,106 +2,17 @@ const fs = require("fs");
 const path = require("path");
 const PizZip = require("pizzip");
 const Docxtemplater = require("docxtemplater");
-const os = require("os");
-require("dotenv").config(); // Добавьте это в самом начале файла
+require("dotenv").config();
 
 const { put } = require("@vercel/blob");
-
-const NAME_FILE_MAP = {
-  Institutional_v250507: "Institutional_v250507.docx",
-  Departmental_v250507: "Departmental_v250507.docx",
-  MSA_v250321: "MSA_v250321.docx",
-  SOW_v250321: "SOW_v250321.docx",
-  SOW_v250609: "SOW_v250609.docx",
-};
-
-function splitProducts(products) {
-  const eras_programs = [];
-  const nonErasMap = new Map();
-
-  for (const product of products) {
-    const props = product.properties || {};
-    const isEras = props.eras_program__sync_ === "true";
-    const acgme_id = props.associated_program_id__sync_ || "";
-    const specialty = props.specialty || "";
-    const ts_id = props.thalamus_core_id__sync_ || "";
-    const cost = parseFloat(props.cost) || 0;
-    const name = (props.product_name || "").toLowerCase();
-
-    if (isEras) {
-      eras_programs.push({
-        count: 1,
-        acgme_id,
-        specialty,
-        ts_id,
-        v_price: cost,
-        t_price: cost,
-      });
-    } else {
-      if (!nonErasMap.has(acgme_id)) {
-        nonErasMap.set(acgme_id, {
-          count: 0,
-          acgme_id,
-          specialty,
-          ts_id,
-          c_price: 0,
-          v_price: 0,
-          t_price: 0,
-        });
-      }
-
-      const entry = nonErasMap.get(acgme_id);
-
-      entry.count += 1;
-
-      if (name.includes("core")) {
-        entry.c_price = cost;
-      } else if (name.includes("video")) {
-        entry.v_price = cost;
-      }
-
-      entry.t_price = entry.c_price + entry.v_price;
-    }
-  }
-
-  const non_eras_programs = Array.from(nonErasMap.values());
-
-  return { eras_programs, non_eras_programs };
-}
-
-function getProductStats(products = []) {
-  return products.reduce(
-    (acc, product) => {
-      const { cost, eras_program__sync_ } = product.properties || {};
-      const numericCost = parseFloat(cost) || 0;
-      const isEras = eras_program__sync_ === "true";
-
-      acc.all_count += 1;
-      acc.total += numericCost;
-
-      if (isEras) {
-        acc.e_sum += numericCost;
-        acc.eras_count += 1;
-      } else {
-        acc.ne_sum += numericCost;
-        acc.non_eras_count += 1;
-      }
-
-      return acc;
-    },
-    {
-      e_sum: 0,
-      ne_sum: 0,
-      all_count: 0,
-      eras_count: 0,
-      non_eras_count: 0,
-      total: 0,
-    }
-  );
-}
+const {
+  NAME_FILE_MAP,
+  splitProducts,
+  getProductStats,
+} = require("../utils/helpers");
 
 module.exports = async (req, res) => {
-  const { doc_name, data } = req.body;
+  const { deal_name = "", doc_name, data } = req.body;
 
   const { street, city, state, zip } = data.address || {};
   const parts = [street, city, state, zip].filter(Boolean);
@@ -151,17 +62,21 @@ module.exports = async (req, res) => {
     doc.render(dataForDocument);
 
     const buffer = doc.getZip().generate({ type: "nodebuffer" });
-    const outputFilename = `Talamus_quote_${doc_name}_${Date.now()}.docx`;
+    const fomrated_date = new Date().toLocaleDateString("en-US", {
+      month: "2-digit",
+      day: "2-digit",
+      year: "numeric",
+    });
+    const outputFilename = `${deal_name}_${doc_name}_${fomrated_date}.docx`;
 
     const blob = await put(outputFilename, buffer, {
       access: "public",
       addRandomSuffix: false,
+      allowOverwrite: true,
       contentType:
         "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
       token: process.env.BLOB_READ_WRITE_TOKEN,
     });
-
-    console.log("blob", blob);
 
     return res.send({
       success: true,
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,4 +1,4 @@
-export const NAME_FILE_MAP = {
+const NAME_FILE_MAP = {
   Institutional_v250507: "Institutional_v250507.docx",
   Departmental_v250507: "Departmental_v250507.docx",
   MSA_v250321: "MSA_v250321.docx",
@@ -6,7 +6,61 @@ export const NAME_FILE_MAP = {
   SOW_v250609: "SOW_v250609.docx",
 };
 
-export function getProductStats(products = []) {
+function splitProducts(products) {
+  const eras_programs = [];
+  const nonErasMap = new Map();
+
+  for (const product of products) {
+    const props = product.properties || {};
+    const isEras = props.eras_program__sync_ === "true";
+    const acgme_id = props.associated_program_id__sync_ || "";
+    const specialty = props.specialty || "";
+    const ts_id = props.thalamus_core_id__sync_ || "";
+    const cost = parseFloat(props.cost) || 0;
+    const name = (props.product_name || "").toLowerCase();
+
+    if (isEras) {
+      eras_programs.push({
+        count: 1,
+        acgme_id,
+        specialty,
+        ts_id,
+        v_price: cost,
+        t_price: cost,
+      });
+    } else {
+      if (!nonErasMap.has(acgme_id)) {
+        nonErasMap.set(acgme_id, {
+          count: 0,
+          acgme_id,
+          specialty,
+          ts_id,
+          c_price: 0,
+          v_price: 0,
+          t_price: 0,
+        });
+      }
+
+      const entry = nonErasMap.get(acgme_id);
+
+      entry.count += 1;
+
+      if (name.includes("core")) {
+        entry.c_price = cost;
+      } else if (name.includes("video")) {
+        entry.v_price = cost;
+      }
+
+      entry.t_price = entry.c_price + entry.v_price;
+    }
+  }
+
+  const non_eras_programs = Array.from(nonErasMap.values());
+
+  return { eras_programs, non_eras_programs };
+}
+
+function getProductStats(products = []) {
   return products.reduce(
     (acc, product) => {
       const { cost, eras_program__sync_ } = product.properties || {};
@@ -14,6 +68,7 @@ export function getProductStats(products = []) {
       const isEras = eras_program__sync_ === "true";
 
       acc.all_count += 1;
+      acc.total += numericCost;
 
       if (isEras) {
         acc.e_sum += numericCost;
@@ -31,6 +86,13 @@ export function getProductStats(products = []) {
       all_count: 0,
       eras_count: 0,
       non_eras_count: 0,
+      total: 0,
     }
   );
 }
+
+module.exports = {
+  NAME_FILE_MAP,
+  splitProducts,
+  getProductStats,
+};
